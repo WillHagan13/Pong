929714d4741640e59cb37c01dccb0a464d397cf0
WillHagan13 <wlhagan13@gmail.com>
2025-01-02 14:29:34 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("Lazer.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("Score.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("Brown.mp3");
